226bf459f3cec52dba0a62d40e7d1050eac33212
barbara <barbara@net4wing.com>
2026-04-06 22:43:15 +0200

    user login-logout

--- config/settings.py
+++ config/settings.py
@@ -115,6 +115,10 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+LOGIN_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
